7ad54dceb37a608499ff3eb9cbcc4a0b65ffc068
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-11-03 14:29:53 +0700

    build: initialize socket.io server

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const cors = require('cors');
 
 app.use(cors());
 
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
+
+io.on('connection', socket => {
+  console.log(`🟢: ${socket.id} online`);
+  io.on('disconnect', () => {
+    console.log(`🔴: ${socket.id} offline`);
+  });
+});
+
 app.get('/api', (req, res) => {
   res.json({
     message: 'Welcome to the API',
